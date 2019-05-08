b1a7eb5bb67babe66b41c37c5abff40c32d83b24
Joe Abbott <jwa7@uw.edu>
2019-05-08 12:16:57 -0700

    creating unit tests for organize_data.py script

---
+++ expressyeaself/tests/test_organize_data.py
@@ -0,0 +1,13 @@
+"""
+This script contains the unit tests for the functions found in
+the organize_data.py script.
+"""
+
+def test_split_scaffolds_by_type():
+	"""
+	Tests the function that splits an input excel file of scaffold
+	data by their type, outputting the different types to different
+	output files.
+	"""
+
+	return
