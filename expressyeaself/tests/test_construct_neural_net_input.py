b0768c06e7ece9ad0d0b46b5edc2e371ab8de39c
Joe Abbott <ja16478@bristol.ac.uk>
2019-05-16 00:17:36 -0700

    writing unit test stub for wrapper function

---
+++ expressyeaself/tests/test_construct_neural_net_input.py
@@ -0,0 +1,12 @@
+"""
+A script containing unit tests for the functions in the
+construct_neural_net_input.py script.
+"""
+
+def test_get_input_data():
+	"""
+	Tests the function that creates the appropriate input
+	data for a neural network architecture.
+	"""
+
+	return
