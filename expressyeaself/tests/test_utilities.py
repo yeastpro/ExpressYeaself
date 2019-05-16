4ab4484b64e0c2ccf3593dffdeba5e66cf4244eb
Joe Abbott <ja16478@bristol.ac.uk>
2019-05-16 00:14:08 -0700

    adding unit test for smart open function

---
+++ expressyeaself/tests/test_utilities.py
@@ -0,0 +1,12 @@
+"""
+This script contains the unit tests for the functions found in
+the utilities.py script.
+"""
+
+def test_smart_open():
+	"""
+	Tests the function that decompresses (if compressed) and opens
+	a file.
+	"""
+
+	return
