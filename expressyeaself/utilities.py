14decda0a926b7d4f16f5f0dcf371696aebda6cb
Joe Abbott <ja16478@bristol.ac.uk>
2019-05-16 00:14:48 -0700

    creating utilities script and writing function that smart opens raw data files

---
+++ expressyeaself/utilities.py
@@ -0,0 +1,40 @@
+"""
+This script contains utility functions that are useful in areas
+of the project.
+"""
+import gzip
+
+def smart_open(filename, mode='rt'):
+	"""
+	A function to open a file. If the file is compressed
+	(with a '.gz' extension) it is decompressed before being
+	opened. Otherwise, the function is opened normally.
+
+	Args:
+	-----
+		filename (str) -- the absolute pathname of the input
+		file to be opened.
+
+		mode (str) -- the way in which the file should be
+		opened by the gzip.open command. Options: 'r', 'rb',
+		'a', 'ab', 'w', 'wb', 'x' or 'xb' for binary files,
+		and 'rt', 'at', 'wt', or 'xt' for text files.
+		Default: 'rt'.
+		
+	Returns:
+	-----
+		opened_file (file type) -- the opened file.
+
+	** Adapted from Carl de Boer's function in https://github.com/Carldeboer/CisRegModels/blob/master/CisRegModels/MYUTILS.py **
+	"""
+	# Assertions
+	assert isinstance(filename, str), 'Input file pathname must be a string.'
+	assert isinstance(mode, str), 'Opening mode must be passed as a string.'
+	assert os.path.exists(filename), 'Input file does not exist.'
+	# Functionality
+	if len(filename) > 3 and filename[-3:].lower() == '.gz':
+		file = gzip.open(filename, mode)
+	else:
+		file = open(filename,mode);
+
+	return file
