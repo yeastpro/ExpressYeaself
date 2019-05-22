bc9ccca44a5fc6fde0a6fefce76feb153dc2df51
Joe Abbott <ja16478@bristol.ac.uk>
2019-05-21 18:20:27 -0700

    writing function to encode all sequences in a given input file

--- expressyeaself/encode_sequences.py
+++ expressyeaself/encode_sequences.py
@@ -6,12 +6,14 @@ Methods by which sequences can be encoded: One-Hot.
 """
 import numpy as np
 import organize_data as organize
+import os
+from utilities import smart_open as smart_open
 from utilities import get_time_stamp as get_time_stamp
 
 BASES = ['A','T','G','C']
 METHODS = ['One-Hot']
 
-def encode_sequence_with_method(input_seqs, method='One-Hot',
+def encode_sequences_with_method(input_seqs, method='One-Hot',
 								pad_sequences=True, extra_padding=0,
 								pad_front=False):
 	"""
@@ -49,7 +51,7 @@ def encode_sequence_with_method(input_seqs, method='One-Hot',
 		separated.
 	"""
 	# Assertions
-	assert isinstance(input_seq, str), 'TypeError: Input file path must be \
+	assert isinstance(input_seqs, str), 'TypeError: Input file path must be \
 	passed as a string.'
 	assert isinstance(method, str), 'TypeError: Specified method must be a \
 	a string.'
@@ -58,7 +60,8 @@ def encode_sequence_with_method(input_seqs, method='One-Hot',
 	# Functionality
 	# Define absolute path of output file
 	time_stamp = get_time_stamp() # get time stamp for unique file naming
-	relative_path = '../example/encoded_data/' + time_stamp
+	relative_path = ('../example/encoded_data/' + time_stamp +
+		'_encoded_sequences')
 	if pad_sequences:
 		if pad_front:
 			relative_path += '_front_padded'
@@ -66,31 +69,55 @@ def encode_sequence_with_method(input_seqs, method='One-Hot',
 			relative_path += '_end_padded'
 	else:
 		relative_path += '_unpadded'
- 	if extra_padding != 0:
+	if extra_padding != 0:
 		relative_path += '_extra_%s' %(extra_padding)
 	else:
-		continue
-	relative_path += '_encoded_sequences.txt'
+		pass
+	relative_path += '.txt'
 	absolute_path = os.path.join(os.getcwd(), relative_path)
 	# Open input and output files
-	input_seqs = smart_open(input_seqs, 'rt')
-	outfile = smart_open(absolute_path, 'wt')
+	infile = smart_open(input_seqs, 'r')
+	# outfile = smart_open(absolute_path, 'w')
 	# Encode and pad each sequence and write it to file with expression level
 	max_length, _, _ = organize.get_max_min_mode_length_of_seqs(input_seqs)
-	for line in input_seqs:
+	line_number = 1
+	out_dict = {}
+	for line in infile:
+		line_number += 1
+		line = organize.check_valid_line(line)
+		if line == 'skip_line':
+			continue
 		seq, exp_level = organize.separate_seq_and_el_data(line)
-		if method = 'One-Hot':
-			encoded_seq = one_hot_encode_sequence(seq)
-			if pad_sequences:
-				encoded_seq = resize_array(encoded_seq,
-									max_length=(max_length + extra_padding),
-									edit_front=pad_front)
-		outfile.write(encoded_seq + '\t' + exp_level + '\r\n')
+		# Encode
+		if method == 'One-Hot':
+			try:
+				encoded_seq = one_hot_encode_sequence(seq)
+			except Exception:
+				print('Error on line %s' %(line_number))
+				raise AssertionError
+		else:
+			# Another encoding method will go here
+			# encoded_seq = another_encoding_method(seq)
+			pass
+		# Pad
+		if pad_sequences:
+			resize_len = max_length + extra_padding
+		else:
+			resize_len = len(encoded_seq) + extra_padding
+		if len(encoded_seq) == resize_len: # Doesn't need resizing
+			pass
+		else:
+			encoded_seq = resize_array(encoded_seq, resize_to=resize_len,
+										edit_front=pad_front)
+		# Write sequence and EL to file.
+		# outfile.write(str(encoded_seq) + '\t' + str(exp_level) + '\n')
+		out_dict[line_number] = (encoded_seq, exp_level)
 	# Close the input and output files
-	input_seqs.close()
-	outfile.close()
+	infile.close()
+	# outfile.close()
 
-	return absolute_path
+	# return absolute_path
+	return out_dict
 
 def one_hot_encode_sequence(promoter_seq):
 	"""
@@ -111,71 +138,78 @@ def one_hot_encode_sequence(promoter_seq):
 	sequence must be a string.'
 	non_ATGC_indices = []
 	index = -1 # Iterator for character index in promoter_seq string
+	valid_nucs = ['A', 'T', 'G', 'C', 'N']
 	for nuc in promoter_seq:
-	    index += 1
-	    if nuc not in BASES:
-	        non_ATGC_indices.append(index) # Appends list of incorrect indices
+		index += 1
+		if nuc not in valid_nucs:
+			non_ATGC_indices.append(index) # Appends list of incorrect indices
 	if len(non_ATGC_indices) is not 0:
-	    raise Exception('Input nucleotide sequence contains a non ATGC base at \
+		raise Exception('Input nucleotide sequence contains a non ATGC base at \
 		string indices %s' %(non_ATGC_indices))
 	# Functionality
 	one_hot_seq = []
 	for nuc in promoter_seq:
 		index = -1 # Iterator for index in BASES list
 		nuc_vector = []
-		for base in BASES:
-			index += 1
-			if nuc == base:
-				nuc_vector.append(1)
-			else:
-				nuc_vector.append(0)
+		if nuc == 'N':
+			nuc_vector = [0,0,0,0] # add a null vector
+		else:
+			for base in BASES:
+				index += 1
+				if nuc == base:
+					nuc_vector.append(1)
+				else:
+					nuc_vector.append(0)
 		one_hot_seq.append(nuc_vector)
 
 	return one_hot_seq
 
-def resize_array(input_array, max_length=223, edit_front=False):
+def resize_array(input_array, resize_to=None, edit_front=False):
 	"""
-	Takes an M x N 2D array (where M is the length to resize) and
-	resizes it to the specified maximum length, adding (if too short)
-	null vectors ([0,0,0,0,...] of length N) or removing
-	base vectors (if too long) from either the front or end of the
-	sequence so that the output encoded array is of dimensions
-	(max_length) x N.
+	Takes an M x N 2D array (where M is the length to edit) and
+	resizes it to the specified resize_to length, adding (if too
+	short) null vectors ([0,0,0,0,...] of length N) or removing
+	base vectors (if too long) from either the front or end of
+	the sequence so that the output encoded array is of dimensions
+	(resize_to) x N.
 
 	Args:
 	-----
 		input_array (list) -- the one-hot encoded binary 2D array
 		to resize.
 
-		max_length (int) -- the length to resize the array to.
+		resize_to (int) -- the length to resize the array to.
+		Default: None (returns the input array with no resizing).
 
 		edit_front (bool) -- whether to add/remove binary base
-		vectors from the front or back of the array. Default: False.
+		vectors from the front or back of the array. Default:
+		False (so removes them from the back).
 
 	Returns:
 	-----
 		input_array (list) -- the resized array of dimensions
-		(max_length)x4.
+		(resize_to)x4.
 	"""
 	# Assertions
 	assert isinstance(input_array, list), 'Input array must be a list.'
-	assert isinstance(max_length, int), 'Max length must be an integer.'
+	assert isinstance(resize_to, (int, type(None))), 'Length to resize the \
+	array to must be an integer or None.'
 	assert isinstance(edit_front, bool), 'TypeError: edit_front must be a bool.'
 	previous_len = len(input_array[0])
 	for i in range(1, len(input_array)): # check vectors in array same length
-	    current_len = len(input_array[i])
-	    assert current_len == previous_len, 'Not all vectors in input array \
+		current_len = len(input_array[i])
+		assert current_len == previous_len, 'Not all vectors in input array \
 		are of the same length.'
-	    previous_len = current_len
+		previous_len = current_len
 	# Functionality
-	len_diff = len(input_array) - max_length
+	len_diff = len(input_array) - resize_to
 	if len_diff == 0: # doesn't need resizing
 		return input_array
 	elif len_diff > 0: # Sequence needs trimming
 		if edit_front:
-			return input_array[-max_length:]
+			return input_array[-resize_to:]
 		else:
-			return input_array[:max_length]
+			return input_array[:resize_to]
 	elif len_diff < 0: # Sequence needs filling
 		# Ensuring null vector to be added matches dimensions of input array
 		null_vect = [0] * len(input_array[0])
