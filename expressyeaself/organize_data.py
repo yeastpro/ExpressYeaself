cf2c696a621c39501f8221efb4ad0cccd240adda
Joe Abbott <ja16478@bristol.ac.uk>
2019-05-24 13:56:02 -0700

    cleaning up functions

--- expressyeaself/organize_data.py
+++ expressyeaself/organize_data.py
@@ -3,133 +3,104 @@ This script contains functions to organize and split up data based
 on several experimental parameters.
 """
 import os
-import matplotlib as plt
 import pandas as pd
 from utilities import smart_open as smart_open
 from utilities import get_time_stamp as get_time_stamp
 import xlrd
 
-def split_scaffolds_by_type(infile):
+def separate_seq_and_el_data(line):
 	"""
-	Splits the scaffold data contained within an Excel file
-	found at https://www.biorxiv.org/highwire/filestream/85247/
-	field_highwire_adjunct_files/1/224907-2.xlsxin by scaffold
-	type, creating different output files for each type that
-	contain all the scaffold IDs and sequences (tab separated).
-	Output files are created in the ExpressYeaself/example
-	directory.
+	Takes a string containing a nucleotide sequence and its expression
+	level (el) - tab separated - and returns the sequence as a string
+	and the expression level as a float.
 
 	Args:
 	-----
-		scaff_infile (str) -- the absolute path for the input file
-		containing all the scaffold data.
+		line (str) -- the input line containing the sequence and
+		expression level (tab separated) to be separated.
 
 	Returns:
 	-----
-		None
+		seq (str) -- the nucleotide sequence.
+
+		exp_level (float) -- the expression level of the sequence.
 	"""
 	# Assertions
-	assert isinstance(infile, str), 'TypeError: input file pathname \
-	must be a string.'
-	assert os.path.exists(infile), 'Input file does not exist.'
+	assert isinstance(line, str), 'Input line must be passed as a string.'
 	# Functionality
-	scaff_df = pd.read_excel(infile, index_col = 'Scaffold ID')
-	types = scaff_df['Scaffold type'].unique()
-	for type in types:
-		# Create a new output file for each unique type of scaffold
-		relative_path = '../example/' + type + '_scaffolds.txt'
-		absolute_path = os.path.join(os.getcwd(), relative_path)
-		outfile = smart_open(absolute_path, 'w')
-		# Reduce scaffold data to only data of the current type
-		type_df = scaff_df[scaff_df['Scaffold type'] == type]
-		for index, row in type_df.iterrows():
-			outfile.write(index + '\t' + row['Sequence'] + '\n')
-		outfile.close()
+	data = line.rstrip().split('\t')
+	seq = data[0]
+	exp_level = float(data[1])
 
-	return
+	return seq, exp_level
 
-def check_oligonucleotide_flanks(seq_infile, scaffold_type):
+def check_valid_line(line):
 	"""
-	Checks that all the oligonucleotide sequences in an input file
-	consist of the same sequences that flank the variable 80-mer
-	sequence. i.e. all sequences in the input file should be of the
-	form:
-	TGCATTTTTTTCACATC-(variable region)-GTTACGGCTGTT
-	Whereas the input sequences measured in the Abf1TATA scaffold
-	will be of the form:
-	TCACGCAGTATAGTTC-(variable region)-GGTTTATTGTTTATAAAAA
-	These flanking sequences are for in-lab sequencing purposes only,
-	so can be discarded when the 80-mer variable sequences are
-	inserted into the a scaffold sequence.
+	Takes an line from an input file containing sequence and
+	expression level data and returns instructions on what to
+	do based on its classification. For example, if the line is
+	a comment or is empty, the function will return 'skip_line'.
+	If the line is encoded into bytes, it will return the
+	decoded line. Not satisfying these conditionals will mean the
+	line is valid, and so will be returned as it was inputted.
 
 	Args:
 	-----
-		seq_infile (str) -- the absolute path of the input file
-		containing all of the oligonucleotide sequences to be
-		checked, and their expression level values (tab separated).
-
-		scaffold_type (str) -- the scaffold type (pTpA or Abf1TATA)
-		in which the expression levels for the sequences in the
-		input file were measured.
+		line (str or bytes) -- a line from an input file to be
+		checked for validity
 
 	Returns:
 	-----
-		incorrect_lines (list) -- returns a list of line numbers for
-		for sequences that contain incorrect flank sequences.
+		line (str) - if the input line was valid, the decoded line
+		is returned. Otherwise, the string 'skip_line' will be
+		returned.
+	"""
+	if isinstance(line, bytes): # decodes line if encoded
+		line = line.decode()
+	if line is None or line == "" or line[0]=="#":
+		return 'skip_line'
+	try:
+		seq, exp_level = separate_seq_and_el_data(line)
+	except IndexError:
+		line = 'skip_line'
+
+	return line
+
+def get_max_min_mode_length_of_seqs(input_seqs):
+	"""
+	Returns the maximum, minimum, and modal length of the sequences
+	in a file containing input sequences.
+
+	Args:
+	-----
+		input_seqs (str) -- the absolute path of the file containing
+		the input sequences and their expression levels, tab separated.
+
+	Returns:
+	-----
+		max_length (int) -- the length of the longest sequence in the
+		input file.
 	"""
 	# Assertions
-	assert isinstance(seq_infile, str), 'Absolute pathname must be passed \
-	as a string.'
-	assert isinstance(scaffold_type, str), 'Scaffold type must be passed as a \
-	string.'
-	assert scaffold_type == 'pTpA' or scaffold_type == 'Abf1TATA', 'Scaffold \
-	type must be specified as either pTpA or Abf1TATA.'
+	assert isinstance(input_seqs, str), 'Path name for input file must be \
+	passed as a string.'
+	assert os.path.exists(input_seqs), 'Input file does not exist.'
 	# Functionality
-	if scaffold_type == 'pTpA':
-		flank_A = 'TGCATTTTTTTCACATC'
-		flank_B = 'GGTTACGGCTGTT'
-	elif scaffold_type == 'Abf1TATA':
-		flank_A = 'TCACGCAGTATAGTTC'
-		flank_B = 'GGTTTATTGTTTATAAAAA'
-	infile = smart_open(seq_infile, 'r')
-	line_number = 0
-	incorrect_lines = []
+	infile = smart_open(input_seqs, 'r')
+	seq_lengths = []
 	for line in infile:
-		line_number += 1
 		line = check_valid_line(line)
 		if line == 'skip_line':
 			continue
 		seq, exp_level = separate_seq_and_el_data(line)
-		if seq.startswith(flank_A) and seq.endswith(flank_B):
-			pass
-		else:
-			incorrect_lines.append(line_number)
+		seq_lengths.append(len(seq))
+	max_length = max(seq_lengths)
+	min_length = min(seq_lengths)
+	modal_length = max(set(seq_lengths), key=seq_lengths.count)
+	# Close the input file.
+	infile.close()
 
-	return incorrect_lines
-
-def create_proxy_one_hot_data(data_size=100, scaffold_type='pTpA'):
-	"""
-	Creates a small data set (of size data_size) of one-hot-encoded
-	sequences, where each sequence is of the number of vectors in
-	length specified by the scaffold type.
-	For pTpA, the length of each sequence is 223 = 54 + 80 + 89,
-	where 80 bp is the variable seq and the 54 and 89 bp either side
-	is the pTpA scaffold
-	For Abf1TATA, the length of each sequence is 245 = 60 + 80 + 105.
-
-	Args:
-	-----
-		data_size (int) -- the number of data points in the
-		sample data set.
-
-		scaffold_type (str) -- the scaffold type (either pTpA or
-		Abf1TATA) to mimic the data on.
-
-	Returns:
-	-----
-		sample_data (list) -- the sample one-hot encoded data.
-	"""
-	return
+	return max_length, min_length, modal_length
 
 def pull_homogeneous_seqs(input_seqs, scaffold_type=None):
 	"""
@@ -201,67 +172,64 @@ def pull_homogeneous_seqs(input_seqs, scaffold_type=None):
 
 	return absolute_path
 
-def separate_seq_and_el_data(line):
+def check_oligonucleotide_flanks(seq_infile, scaffold_type):
 	"""
-	Takes a string containing a nucleotide sequence and its expression
-	level (el) - tab separated - and returns the sequence as a string
-	and the expression level as a float.
+	Checks that all the oligonucleotide sequences in an input file
+	consist of the same sequences that flank the variable 80-mer
+	sequence. i.e. all sequences in the input file should be of the
+	form:
+	TGCATTTTTTTCACATC-(variable region)-GTTACGGCTGTT
+	Whereas the input sequences measured in the Abf1TATA scaffold
+	will be of the form:
+	TCACGCAGTATAGTTC-(variable region)-GGTTTATTGTTTATAAAAA
+	These flanking sequences are for in-lab sequencing purposes only,
+	so can be discarded when the 80-mer variable sequences are
+	inserted into the a scaffold sequence.
 
 	Args:
 	-----
-		line (str) -- the input line containing the sequence and
-		expression level (tab separated) to be separated.
+		seq_infile (str) -- the absolute path of the input file
+		containing all of the oligonucleotide sequences to be
+		checked, and their expression level values (tab separated).
+
+		scaffold_type (str) -- the scaffold type (pTpA or Abf1TATA)
+		in which the expression levels for the sequences in the
+		input file were measured.
 
 	Returns:
 	-----
-		seq (str) -- the nucleotide sequence.
-
-		exp_level (float) -- the expression level of the sequence.
+		incorrect_lines (list) -- returns a list of line numbers for
+		for sequences that contain incorrect flank sequences.
 	"""
 	# Assertions
-	assert isinstance(line, str), 'Input line must be passed as a string.'
+	assert isinstance(seq_infile, str), 'Absolute pathname must be passed \
+	as a string.'
+	assert isinstance(scaffold_type, str), 'Scaffold type must be passed as a \
+	string.'
+	assert scaffold_type == 'pTpA' or scaffold_type == 'Abf1TATA', 'Scaffold \
+	type must be specified as either pTpA or Abf1TATA.'
 	# Functionality
-	data = line.rstrip().split('\t')
-	seq = data[0]
-	exp_level = float(data[1])
-
-	return seq, exp_level
-
-def get_max_min_mode_length_of_seqs(input_seqs):
-	"""
-	Returns the maximum, minimum, and modal length of the sequences
-	in a file containing input sequences.
-
-	Args:
-	-----
-		input_seqs (str) -- the absolute path of the file containing
-		the input sequences and their expression levels, tab separated.
-
-	Returns:
-	-----
-		max_length (int) -- the length of the longest sequence in the
-		input file.
-	"""
-	# Assertions
-	assert isinstance(input_seqs, str), 'Path name for input file must be \
-	passed as a string.'
-	assert os.path.exists(input_seqs), 'Input file does not exist.'
-	# Functionality
-	infile = smart_open(input_seqs, 'r')
-	seq_lengths = []
+	if scaffold_type == 'pTpA':
+		flank_A = 'TGCATTTTTTTCACATC'
+		flank_B = 'GGTTACGGCTGTT'
+	elif scaffold_type == 'Abf1TATA':
+		flank_A = 'TCACGCAGTATAGTTC'
+		flank_B = 'GGTTTATTGTTTATAAAAA'
+	infile = smart_open(seq_infile, 'r')
+	line_number = 0
+	incorrect_lines = []
 	for line in infile:
+		line_number += 1
 		line = check_valid_line(line)
 		if line == 'skip_line':
 			continue
 		seq, exp_level = separate_seq_and_el_data(line)
-		seq_lengths.append(len(seq))
-	max_length = max(seq_lengths)
-	min_length = min(seq_lengths)
-	modal_length = max(set(seq_lengths), key=seq_lengths.count)
-	# Close the input file.
-	infile.close()
+		if seq.startswith(flank_A) and seq.endswith(flank_B):
+			pass
+		else:
+			incorrect_lines.append(line_number)
 
-	return max_length, min_length, modal_length
+	return incorrect_lines
 
 def remove_files(files):
 	"""
@@ -290,34 +258,41 @@ def remove_files(files):
 
 	return
 
-def check_valid_line(line):
+def split_scaffolds_by_type(infile):
 	"""
-	Takes an line from an input file containing sequence and
-	expression level data and returns instructions on what to
-	do based on its classification. For example, if the line is
-	a comment or is empty, the function will return 'skip_line'.
-	If the line is encoded into bytes, it will return the
-	decoded line. Not satisfying these conditionals will mean the
-	line is valid, and so will be returned as it was inputted.
+	Splits the scaffold data contained within an Excel file
+	found at https://www.biorxiv.org/highwire/filestream/85247/
+	field_highwire_adjunct_files/1/224907-2.xlsxin by scaffold
+	type, creating different output files for each type that
+	contain all the scaffold IDs and sequences (tab separated).
+	Output files are created in the ExpressYeaself/example
+	directory.
 
 	Args:
 	-----
-		line (str or bytes) -- a line from an input file to be
-		checked for validity
+		scaff_infile (str) -- the absolute path for the input file
+		containing all the scaffold data.
 
 	Returns:
 	-----
-		line (str) - if the input line was valid, the decoded line
-		is returned. Otherwise, the string 'skip_line' will be
-		returned.
+		None
 	"""
-	if isinstance(line, bytes): # decodes line if encoded
-		line = line.decode()
-	if line is None or line == "" or line[0]=="#":
-		return 'skip_line'
-	try:
-		seq, exp_level = separate_seq_and_el_data(line)
-	except IndexError:
-		line = 'skip_line'
+	# Assertions
+	assert isinstance(infile, str), 'TypeError: input file pathname \
+	must be a string.'
+	assert os.path.exists(infile), 'Input file does not exist.'
+	# Functionality
+	scaff_df = pd.read_excel(infile, index_col = 'Scaffold ID')
+	types = scaff_df['Scaffold type'].unique()
+	for type in types:
+		# Create a new output file for each unique type of scaffold
+		relative_path = '../example/' + type + '_scaffolds.txt'
+		absolute_path = os.path.join(os.getcwd(), relative_path)
+		outfile = smart_open(absolute_path, 'w')
+		# Reduce scaffold data to only data of the current type
+		type_df = scaff_df[scaff_df['Scaffold type'] == type]
+		for index, row in type_df.iterrows():
+			outfile.write(index + '\t' + row['Sequence'] + '\n')
+		outfile.close()
 
-	return line
+	return
