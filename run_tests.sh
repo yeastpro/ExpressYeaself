073679eb8369e026ebddffb3309f0b0f2569db9c
Joe Abbott <ja16478@bristol.ac.uk>
2019-06-09 15:02:23 -0700

    ensuring pycache is cleared after running tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-py.test #-s --pyargs expressyeaself --cov-report term-missing --cov=expressyeaself --cov-config .coveragerc
-#rm expressyeaself/tests/__pycache__/*
+py.test -s --pyargs expressyeaself --cov-report term-missing --cov=expressyeaself --cov-config .coveragerc
+rm -f expressyeaself/tests/__pycache__/*
 
