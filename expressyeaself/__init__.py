ad3aa04bd903dbd4fbec90f1634e215fc337e72f
Joe Abbott <jwa7@uw.edu>
2019-06-09 12:34:48 -0700

    changing method of module imports

--- expressyeaself/__init__.py
+++ expressyeaself/__init__.py
@@ -1,9 +1,9 @@
-#from . import build_promoter  # noqa: F401
-#from . import construct_neural_net  # noqa: F401
-#from . import encode_sequences  # noqa: F401
-#from . import organize_data  # noqa: F401
+# from . import build_promoter  # noqa: F401
+# from . import construct_neural_net  # noqa: F401
+# from . import encode_sequences  # noqa: F401
+# from . import organize_data  # noqa: F401
 
-#from expressyeaself import build_promoter  # noqa: F401
-#from expressyeaself import construct_neural_net  # noqa: F401
-#from expressyeaself import encode_sequences  # noqa: F401
-#from expressyeaself import organize_data  # noqa: F401
+# from expressyeaself import build_promoter  # noqa: F401
+# from expressyeaself import construct_neural_net  # noqa: F401
+# from expressyeaself import encode_sequences  # noqa: F401
+# from expressyeaself import organize_data  # noqa: F401
