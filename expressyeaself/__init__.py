2e3fe89a839ba5f5c94497610eff18136ed7f811
Joe Abbott <jwa7@uw.edu>
2019-06-09 12:16:26 -0700

    commenting out imports

--- expressyeaself/__init__.py
+++ expressyeaself/__init__.py
@@ -3,7 +3,7 @@
 #from . import encode_sequences  # noqa: F401
 #from . import organize_data  # noqa: F401
 
-from expressyeaself import build_promoter  # noqa: F401
-from expressyeaself import construct_neural_net  # noqa: F401
-from expressyeaself import encode_sequences  # noqa: F401
-from expressyeaself import organize_data  # noqa: F401
+#from expressyeaself import build_promoter  # noqa: F401
+#from expressyeaself import construct_neural_net  # noqa: F401
+#from expressyeaself import encode_sequences  # noqa: F401
+#from expressyeaself import organize_data  # noqa: F401
