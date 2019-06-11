49f5803a1a15762a486c14766c89fac3215197c3
Keertana Krishnan <kkrish@uw.edu>
2019-06-10 22:39:37 -0700

    Edited indents

--- loc_con_1d.py
+++ loc_con_1d.py
@@ -243,4 +243,3 @@ def model_eval(sequence_matrix, expression_levels, epochs, batch_size):
 
 ^G Get Help    ^O Write Out   ^W Where Is    ^K Cut Text    ^J Justify     ^C Cur Pos     M-U Undo       M-A Mark Text
 ^X Exit        ^R Read File   ^\ Replace     ^U Uncut Text  ^T To Linter   ^_ Go To Line  M-E Redo       M-6 Copy Text
-
