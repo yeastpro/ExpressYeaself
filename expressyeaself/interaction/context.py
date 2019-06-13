248bc9b22d7a9ca8943ce520d715dc957a4612c5
Joe Abbott <ja16478@bristol.ac.uk>
2019-06-13 02:11:09 -0700

    context file needed for proper relative imports of packages

---
+++ expressyeaself/interaction/context.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                '../..')))
+
+import expressyeaself.build_promoter as build_promoter  # noqa: E402,F401
+import expressyeaself.encode_sequences as encode_sequences  # noqa: E402,F401
+import expressyeaself.organize_data as organize_data  # noqa: E402,F401
+import expressyeaself.process_data as process_data  # noqa: E402,F401
+import expressyeaself.utilities as utilities  # noqa: E402,F401
