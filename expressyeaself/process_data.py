ef058572d8f6bc23feac79a7551e9fa792d62a46
Joe Abbott <ja16478@bristol.ac.uk>
2019-06-12 19:18:54 -0700

    including binarization/pulling of percntiles to wrapper function

--- expressyeaself/process_data.py
+++ expressyeaself/process_data.py
@@ -10,19 +10,20 @@ import os
 import time as t
 
 
-def process_raw_data(input_seqs, scaffold_type=None, homogeneous=False,
-                     deflank=True, insert_into_scaffold=True,
-                     extra_padding=0, pad_front=False, report_loss=True,
-                     report_times=True, remove_files=True):
+def process_raw_data(input_seqs, scaffold_type=None, percentile=None,
+                     binarize_els=True, homogeneous=False, deflank=True,
+                     insert_into_scaffold=True, extra_padding=0,
+                     pad_front=False, report_loss=True, report_times=True,
+                     remove_files=True, create_sample_of_size=None):
     """
     A wrapper function that:
     Takes raw data as retrieved from Carl de Boer's publication
     at https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE104878,
     and processes the sequences according to the custom arguments,
-    pads them to same length, and writes them to an output file along
-    with their expression levels (tab separated). The end of the file
-    contains comments specifying the number of sequences in the file
-    and the lengths of the padded sequences.
+    pads them to same length, and writes them to an output file
+    along with their expression levels (tab separated). The end of
+    the file contains comments specifying the number of sequences
+    in the file and the lengths of the padded sequences.
 
     Args:
     -----
@@ -33,6 +34,22 @@ def process_raw_data(input_seqs, scaffold_type=None, homogeneous=False,
         scaffold_type (str) -- the scaffold type (pTpA or Abf1TATA)
         that the input sequences had their expression levels
         measured in.
+
+        percentile (float) -- the proportion of the raw input data
+        to extract from the sequences with the highest and lowest
+        expression levels. i.e if 'percentile=0.1' then the top
+        10 % of sequences with highest expression levels, and the
+        bottom 10 % of sequences with lowest expression levels will
+        be extracted from the raw input data. The resulting data
+        file will contain ~ 20 % of the data as the raw input data.
+
+        binarize_els (bool) -- if (and only if) a 'percentile'
+        value is passed, this argument determines whether the
+        expression level values (Els) will be binarized or not. If
+        True (defualt), sequences with ELs in the top percentile
+        will have their ELs binarized to 1, and sequences with ELs
+        in the bottom percentile will have their ELs binarized
+        to 0.
 
         homogeneous (bool) -- if True, only sequences of modal
         length will be processed. If False, all sequences will be
@@ -53,47 +70,63 @@ def process_raw_data(input_seqs, scaffold_type=None, homogeneous=False,
         side) or end (right hand side) of the sequences. If True,
         pads the front. Default: False (will pad the end).
 
-        report_loss (bool) -- if True, reports the number of lines of
-        data lost at each step in the process. Default: False.
+        report_loss (bool) -- if True, reports the number of lines
+        of data lost at each step in the process. Default: False.
 
-        report_times (bool) -- if True, reports the time each step in
-        the cleaning process takes. Default: False.
+        report_times (bool) -- if True, reports the time each step
+        in the cleaning process takes. Default: False.
 
-        remove_files (bool) -- if True, will remove the intermediate
-        files created in the process of processing raw data. Default:
-        False (i.e. intermediary files will be kept).
+        remove_files (bool) -- if True, will remove intermediate
+        files created in the process of processing raw data.
+        Default: False (i.e. intermediary files will be kept).
+
+        create_sample_of_size (int) -- if a number is passed, a
+        sample of this size will be taken by pseudo-random from
+        the file containing processed data, and written to a
+        separate file.
 
     Returns:
     -----
-        processed_data (str) -- the absolute path for the file containing
-        processed sequences along with their expression levels.
+        processed_data (str) -- the absolute path for the file
+        containing processed sequences along with their expression
+        levels.
     """
     # Assertions
-    assert isinstance(input_seqs, str), 'Input file path name must be \
-    passed as a string.'
+    assert isinstance(input_seqs, str), ('Input file path name must be '
+                                         'passed as a string.')
     assert os.path.exists(input_seqs), 'Input file does not exist.'
-    assert isinstance(scaffold_type, str), 'Scaffold type must \
-    be passed as a string if specified.'
-    assert scaffold_type == 'pTpA' or scaffold_type == 'Abf1TATA', 'Scaffold\
+    assert isinstance(scaffold_type, str), ('Scaffold type must be passed as '
+                                            'a string if specified.')
+    assert scaffold_type == 'pTpA' or scaffold_type == 'Abf1TATA', 'Scaffold \
     type must be specified as either "pTpA" or "Abf1TATA".'
-    assert isinstance(homogeneous, bool), 'The homogeneous argument must be\
-    passed as a bool.'
-    assert isinstance(deflank, bool), 'The deflank argument must be passed\
-    as a bool.'
-    assert isinstance(insert_into_scaffold, bool), 'The insert_into_scaffold\
-    argument must be passed as a bool.'
-    assert isinstance(extra_padding, int), 'The number of extra vectors to\
-    pad each sequence by should be passed as an integer.'
-    assert extra_padding >= 0, 'extra_padding must be passed as a non-\
-    negative integer.'
-    assert isinstance(pad_front, bool), 'The pad_front argument must be\
-    passed as a bool.'
-    assert isinstance(report_loss, bool), 'The report_loss argument must be\
-    passed as a bool.'
-    assert isinstance(report_times, bool), 'The report_times argument must\
-    be passed as a bool.'
-    assert isinstance(remove_files, bool), 'The remove_files argument must\
-    be passed as a bool.'
+    assert isinstance(percentile, (float, type(None))), ('The "percentile" '
+                                                         'argument must be '
+                                                         'passed as a float.')
+    if percentile is not None:
+        assert percentile < 0.5, '"percentile" must be less that 0.5'
+    assert isinstance(homogeneous, bool), ('The homogeneous argument must be '
+                                           'passed as a bool.')
+    assert isinstance(deflank, bool), ('The deflank argument must be passed '
+                                       'as a bool.')
+    assert isinstance(insert_into_scaffold, bool), ('insert_into_scaffold '
+                                                    'argument must be passed '
+                                                    'as a bool.')
+    assert isinstance(extra_padding, int), ('The number of extra vectors to '
+                                            'pad each sequence by should be '
+                                            'passed as an integer.')
+    assert extra_padding >= 0, ('extra_padding must be passed as a non-'
+                                'negative integer.')
+    assert isinstance(pad_front, bool), ('The pad_front argument must be '
+                                         'passed as a bool.')
+    assert isinstance(report_loss, bool), ('The report_loss argument must be '
+                                           'passed as a bool.')
+    assert isinstance(report_times, bool), ('The report_times argument must '
+                                            'be passed as a bool.')
+    assert isinstance(remove_files, bool), ('The remove_files argument must '
+                                            'be passed as a bool.')
+    if create_sample_of_size is not None:
+        assert isinstance(create_sample_of_size, int), ('Sample size must be '
+                                                        'passed as an int')
     # Functionality
     print('Starting processing of raw data...')
     raw_data = input_seqs
@@ -113,6 +146,27 @@ def process_raw_data(input_seqs, scaffold_type=None, homogeneous=False,
         t0 = t_init
     if remove_files:
         created_files = []  # keep track of the intermediate files created.
+    # Pull out the top and bottom percentiles of data
+    if percentile is not None:
+        print('Pulling out the top and bottom percentiles...')
+        df = organize.sort_by_exp_level(input_seqs)
+        df = organize.discard_mid_data(df, percentile=percentile)
+        processed_data += '_percentiles'
+        if binarize_els:
+            print('Binarizing expression levels...')
+            df = organize.binarize_data(df)
+            processed_data += '_els_binarized'
+        input_seqs = organize.write_df_to_file(df)
+        if report_loss:
+            loss_report['Percentile Seqs'] = get_seq_count(input_seqs)
+        if report_times:
+            t1 = t.time()
+            text = '\tFile created in %s s' % (t1 - t0)
+            print(text)
+            report.write('Top & bottom percentiles pulled...\n' + text + '\n')
+            t0 = t1
+        if remove_files:
+            created_files.append(input_seqs)
     # Create new file of only homogeneous (same length) seqs
     if homogeneous:
         print('Pulling homogeneous sequences from input file...')
@@ -164,8 +218,11 @@ def process_raw_data(input_seqs, scaffold_type=None, homogeneous=False,
         if remove_files:
             created_files.append(input_seqs)
     # Pad sequences
-    print('Padding sequences...')
-    input_seqs = build.pad_sequences(input_seqs, pad_front=pad_front,
+    if homogeneous and extra_padding == 0:
+        pass
+    else:
+        print('Padding sequences...')
+        input_seqs = build.pad_sequences(input_seqs, pad_front=pad_front,
                                      extra_padding=extra_padding)
     if not homogeneous:  # then they will have been padded
         processed_data += '_padded_at'
@@ -196,7 +253,8 @@ def process_raw_data(input_seqs, scaffold_type=None, homogeneous=False,
         text = ('\nRaw data successfully processed.\nLocation: %s\n'
                 % (processed_data))
         print(text)
-        report.write(text)
+        if report_loss or report_times:
+            report.write(text)
     else:  # If no processing was performed.
         text = '\nNo processing performed.\n'
         text += 'Change processing specifications and try again.'
@@ -207,7 +265,8 @@ def process_raw_data(input_seqs, scaffold_type=None, homogeneous=False,
         report.write(text + '\n')
         text = 'Location : %s' % (raw_data)
         print(text)
-        report.write(text + '\n')
+        if report_loss or report_times:
+            report.write(text + '\n')
     # Write the number of seqs and length of seqs to the start of file
     organize.write_num_and_len_of_seqs_to_file(processed_data)
     # Report loss
@@ -235,8 +294,16 @@ def process_raw_data(input_seqs, scaffold_type=None, homogeneous=False,
         text = '\nTotal processing time : %s s' % (t_final - t_init)
         print(text)
         report.write(text)
-        print('Please find the process report in the same directory as the\
-        output file for reports of data losses and timings.')
-    report.close()
+        print('Please find the process report in the same directory as the'
+              ' output file for reports of data losses and timings.')
+    if report_times or report_loss:
+        report.close()
+    # Create sample data
+    if create_sample_of_size is not None:
+        size = create_sample_of_size
+        print('\n\nCreating sample of size %s ...' % str(size))
+        sample_seqs = organize.create_sample_data(processed_data, size)
+        print('\nSample data successfully created.')
+        print('\nLocation: %s \n' % (sample_seqs))
 
     return processed_data
